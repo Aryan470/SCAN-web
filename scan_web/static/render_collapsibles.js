1fa22cb454055e4dda4c1e2faeda01bf9050366e
Aryan470 <aryankhatri2012@gmail.com>
2020-07-25 20:40:02 +0000

    Added css dropdown arrows
    Fixes Aryan470/SCAN-web#66

--- scan_web/static/render_collapsibles.js
+++ scan_web/static/render_collapsibles.js
@@ -1,22 +1,31 @@
 function toggleCollapseNext(event) {
-    console.log("toggled!");
-    var next = event.target.nextElementSibling;
-    console.log(event.target);
+    var label = event.target;
+    if (label.tagName === "I") {
+        label = label.parentElement;
+    }
+
+    var currDir = label.firstElementChild.classList[1];
+    if (currDir === "right") {
+        label.firstElementChild.classList = "arrow down";
+    } else {
+        label.firstElementChild.classList = "arrow right";
+    }
+    var next = label.nextElementSibling;
     if (next.style.display === "none") {
         next.style.display = "";
-        event.target.innerText = "▼ " + event.target.innerText.substring(1)
     } else {
         next.style.display = "none";
-        event.target.innerText = "▶" + event.target.innerText.substring(1);
     }
 }
 
 // These are headings that, when clicked, should have their next sibling's display set to none
 var collapsibles = document.getElementsByClassName("collapsible");
 for (i = 0; i < collapsibles.length; i++) {
-    if (!collapsibles[i].innerText.startsWith("▶")) {
-        collapsibles[i].innerText = "▶ " + collapsibles[i].innerText;
-    }
+    collapsibles[i].innerText = " " + collapsibles[i].innerText;
+    var newArrow = document.createElement("i");
+    newArrow.classList = "arrow right"
+    collapsibles[i].prepend(newArrow);
+
     collapsibles[i].nextElementSibling.style.display = "none";
     collapsibles[i].addEventListener("click", toggleCollapseNext);
 }
