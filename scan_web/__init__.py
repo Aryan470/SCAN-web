d969cf46c97a5b5156a8cbb87eecc33eeb9f30f4
Aryan470 <aryankhatri2012@gmail.com>
2020-08-09 20:55:31 +0000

    Added directory
    Fixes Aryan470/SCAN-web#118

--- scan_web/__init__.py
+++ scan_web/__init__.py
@@ -8,7 +8,7 @@ firebase_admin.initialize_app(cred)
 fireClient = firestore.client()
 
 from flask import Flask, render_template, session, redirect, request, url_for
-from scan_web.blueprints import bakesale, auth, sms
+from scan_web.blueprints import bakesale, auth, sms, info
 # from scan_web import product_management
 
 def create_app():
@@ -25,9 +25,11 @@ def create_app():
         app.register_blueprint(bakesale.bakesale, subdomain="bakesale")
         app.register_blueprint(auth.auth, subdomain="auth")
         app.register_blueprint(sms.sms, subdomain="sms")
+        app.register_blueprint(info.info, subdomain="info")
     app.register_blueprint(bakesale.bakesale, url_prefix="/bakesale")
     app.register_blueprint(auth.auth, url_prefix="/auth")
     app.register_blueprint(sms.sms, url_prefix="/sms")
+    app.register_blueprint(info.info, url_prefix="/info")
 
     @app.route("/")
     def index():
